ae8758faaed258f3b98fa17cec595abd6765dc88
Marcelo Ferreira <joamarcelo@gmail.com>
2016-07-13 19:32:18 -0300

    Add Range
    Description: Add range class.
    
    Commit: Add

---
+++ src/util/Range.java
@@ -0,0 +1,38 @@
+package util;
+
+public class Range {
+
+    private int numberOfDimensions = 0;
+    private int[] min;
+    private int[] max;
+
+    public static Range OneDimensionalRange(int min, int max) {
+        assert min <= max;
+        return new Range(1, new int[]{min}, new int[]{max});
+    }
+
+    public static Range TwoDimensionalRange(int xMin, int xMax, int yMin, int yMax) {
+        assert xMin <= xMax && yMin <= yMax;
+        return new Range(2, new int[]{xMin, yMin}, new int[]{xMax, yMax});
+    }
+
+    private Range(int dimensions, int[] min, int[] max) {
+        this.numberOfDimensions = dimensions;
+        this.min = min;
+        this.max = max;
+    }
+
+    public int dimensions() {
+        return numberOfDimensions;
+    }
+
+    public int min(int dimension) {
+        assert dimension < numberOfDimensions;
+        return this.min[dimension];
+    }
+
+    public int max(int dimension) {
+        assert dimension < numberOfDimensions;
+        return this.max[dimension];
+    }
+}
